aea0181ee1241dec66f76cfe4fd4d94e5bd94fd7
Joaquin Villagran <jvillagran410@gmail.com>
2021-06-02 16:53:52 -0300

    Modifique el parrafo del index

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Que onda? 1234
+          Joaquin Villagran
         </p>
         <a
           className="App-link"
